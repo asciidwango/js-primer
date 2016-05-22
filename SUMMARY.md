d939b6475ce6f604dbd7f81a0caea98c28ca8326
azu <azuciao@gmail.com>
2016-05-22 16:16:09 +0900

    feat(comments): コメントについて
    - 一行コメント
    - 複数行コメント
    - HTML-likeコメント
    
    close #16

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,6 +1,7 @@
 - 書籍の読み方
 - [第1章](source/ch1/README.md)
     - [JavaScriptとは](source/ch1/basic/README.md)
+    - [コメント](source/ch1/comments/README.md)
     - 基礎文法
         - 字句構造
         - 型、値、変数
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ var path = require("path");
 var textlint = require('gulp-textlint');
 gulp.task('textlint', function () {
     return gulp.src('./source/**/*.md')
+        .on('error', function(error){
+            console.error(error);
+        })
         .pipe(textlint({
             formatterName: "pretty-error"
-        }));
+        }))
 });
