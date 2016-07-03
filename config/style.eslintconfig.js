172e158f185f95d49d63c58eb9c2d1d5a8d422b4
azu <azuciao@gmail.com>
2016-07-04 01:17:36 +0900

    style(eslint): add eslint config

--- config/style.eslintconfig.js
+++ config/style.eslintconfig.js
@@ -2,13 +2,31 @@
 "use strict";
 module.exports = {
     rules: {
-        "indent": ["error", 4, {
-            "SwitchCase": 1
-        }],
+        "indent": [
+            "error", 4, {
+                "SwitchCase": 1
+            }
+        ],
         "quotes": ["error", "double"],
+        "array-bracket-spacing": [
+            "error",
+            "never"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs"
+        ],
+        "block-spacing": "error",
+        "comma-spacing": "error",
+        "computed-property-spacing": "error",
+        "no-whitespace-before-property": "error",
+        // Object
+        "object-curly-newline": ["off", "always"],
+        "object-curly-spacing": ["off", "always"],
         "key-spacing": "error",
         "keyword-spacing": "error",
         "linebreak-style": ["error", "unix"],
+        "no-multiple-empty-lines": "error",
         "rest-spread-spacing": "error",
         "semi": ["error", "always"],
         "semi-spacing": "error",
@@ -17,19 +35,21 @@ module.exports = {
         "space-in-parens": "error",
         "space-infix-ops": "error",
         "space-unary-ops": "error",
-        "spaced-comment": ["error", "always", {
-            "exceptions": ["-", "="],
-            "markers": [
-                "eslint",
-                "eslint-env",
-                "eslint-disable",
-                "eslint-enable",
-                "eslint-disable-line",
-                "eslint-disable-next-line",
-                "exported",
-                "globals",
-                "istanbul",
-            ],
-        }]
+        "spaced-comment": [
+            "error", "always", {
+                "exceptions": ["-", "="],
+                "markers": [
+                    "eslint",
+                    "eslint-env",
+                    "eslint-disable",
+                    "eslint-enable",
+                    "eslint-disable-line",
+                    "eslint-disable-next-line",
+                    "exported",
+                    "globals",
+                    "istanbul",
+                ],
+            }
+        ]
     }
-}
+};
