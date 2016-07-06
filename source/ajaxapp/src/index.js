65b2e93d808c849b1663b8d23eb5517a420e936e
laco0416 <laco0416+github@gmail.com>
2016-07-06 22:07:20 +0900

    chore: ダブルクオートに修正

--- source/ajaxapp/src/index.js
+++ source/ajaxapp/src/index.js
@@ -1,11 +1,11 @@
 function fetch(url) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+    xhr.open("GET", url);
     return new Promise((resolve, reject) => {
-        xhr.addEventListener('load', (evt) => {
+        xhr.addEventListener("load", (evt) => {
             resolve(evt.target.response);
         });
-        xhr.addEventListener('error', (evt) => {
+        xhr.addEventListener("error", (evt) => {
             reject(evt.target.error);
         });
         xhr.send();
@@ -13,18 +13,18 @@ function fetch(url) {
 }
 
 function escapeHTML(str) {
-    return str.replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#039;');
+    return str.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
 }
 
 function sanitizeHtml(strings, ...values) {
     return strings.map((part, i) => {
         let arg = values[i];
         if (arg) {
-            if (typeof arg === 'string') {
+            if (typeof arg === "string") {
                 return part + escapeHTML(arg);
             } else {
                 return part + `${arg}`;
@@ -32,11 +32,11 @@ function sanitizeHtml(strings, ...values) {
         } else {
             return part;
         }
-    }).join('');
+    }).join("");
 }
 
 function buildUserInfoView(userInfo) {
-    const view = document.createElement('div');
+    const view = document.createElement("div");
     view.innerHTML = sanitizeHtml`
     <h4>${userInfo.name} (@${userInfo.login})</h4>
     <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
@@ -51,13 +51,13 @@ function buildUserInfoView(userInfo) {
 }
 
 function renderUserInfoView(view) {
-    const target = document.getElementById('result');
+    const target = document.getElementById("result");
     target.innerHTML = null;
     target.appendChild(view);
 }
 
 function fetchInfo() {
-    const userId = document.getElementById('userId').value;
+    const userId = document.getElementById("userId").value;
     if (!userId.trim()) {
         return;
     }
