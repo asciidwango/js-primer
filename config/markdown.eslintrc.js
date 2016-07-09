8e575f9c556307eca883b8c8c0ba960de0b80776
azu <azu@users.noreply.github.com>
2016-07-09 19:27:12 +0900

    refactor(npm): npm run-scriptsを整理 (#84)
    - `npm test` から `npm run build` を外した
    - Travis CIでは全てテストするようにした
    - `npm run lint` と `npm run lint:fix` を追加
    - ESLintの設定ファイルをjsに統一
    - gulpfileを削除
    - gulpにあったテストをmochaに移行

---
+++ config/markdown.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "rules": {
+        "no-undef": "off",
+        "no-unused-vars": "off",
+        "no-var": "off"
+    },
+    "plugins": [
+        "markdown"
+    ],
+    "extends": [
+        "./style.eslintrc.js"
+    ]
+};
---
+++ test/invalid-test.js
@@ -0,0 +1,30 @@
+// LICENSE : MIT
+"use strict";
+const assert = require("power-assert");
+const globby = require('globby');
+const fs = require("fs");
+const path = require("path");
+const strictEval = require("strict-eval");
+const sourceDir = path.join(__dirname, "..", "source");
+/**
+ * `*-invalid.js` が実行 または パースエラーとなることをテストする
+ **/
+describe("invalid:js", function() {
+    const files = globby.sync([`${sourceDir}/**/*-invalid.js`, `!${sourceDir}/**/node_modules{,/**}`]);
+    files.forEach(filePath => {
+        const normalizeFilePath = filePath.replace(sourceDir, "");
+        it(`Should be invalid ${normalizeFilePath}`, function() {
+            const content = fs.readFileSync(filePath, "utf-8");
+            try {
+                strictEval(content);
+                throw new Error("NO_REACH_CODE");
+            } catch (error) {
+                // evalしようとしたらエラーになっていることが期待値
+                // "NO_REACH_CODE"になってるのはおかしい
+                assert.notEqual(error.message, "NO_REACH_CODE", `Should be SyntaxError(parse error) or EvalError: ${error.message}
+Please check it:
+    at NotError (${filePath}:1:1)\n`);
+            }
+        });
+    });
+});
