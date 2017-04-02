c5ffcb17fb1f9e6d9613c5634702678e7e122b42
laco0416 <laco0416+github@gmail.com>
2017-04-02 16:08:20 +0900

    chore: レビュー反映

--- source/use-case/nodecli/refactor-and-unittest/src/test/md2html.js
+++ source/use-case/nodecli/refactor-and-unittest/src/test/md2html.js
@@ -1,10 +1,11 @@
 const assert = require("assert");
 const fs = require("fs");
+const path = require("path");
 const md2html = require("../md2html");
 
-it("default options", () => {
-    const sample = fs.readFileSync("test/fixtures/sample.md", "utf8");
-    const expected = fs.readFileSync("test/fixtures/expected.html", "utf8");
+it("converts Markdown to HTML", () => {
+    const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), "utf8");
+    const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected.html"), "utf8");
 
     assert(md2html(sample) === expected);
 });
