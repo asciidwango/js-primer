a97c93045a4f63bc1eedc5ea15dedfa82841bab6
azu <azuciao@gmail.com>
2018-03-21 17:21:33 +0900

    style(todo): fix lint

--- source/use-case/todoapp/cypress/integration/todo-spec.js
+++ source/use-case/todoapp/cypress/integration/todo-spec.js
@@ -3,12 +3,12 @@
  * @param {string} title
  */
 const addNewTodo = (title) => {
-    cy.get('#js-form-input').type(title);
-    cy.get('#js-form').submit();
+    cy.get("#js-form-input").type(title);
+    cy.get("#js-form").submit();
 };
 
-describe('Todo', function() {
-    it('追加と削除', function() {
+describe("Todo", function() {
+    it("追加と削除", function() {
         // https://on.cypress.io/visit
         cy.visit("/");
 
@@ -21,22 +21,22 @@ describe('Todo', function() {
             expect(items).to.have.length(2);
         });
         // 削除
-        cy.get('li > button').each(e => e.click());
+        cy.get("li > button").each(e => e.click());
         cy.get("li").should(items => {
             expect(items).to.have.length(0);
         });
     });
-    it('チェックの付け外し', function() {
+    it("チェックの付け外し", function() {
         // https://on.cypress.io/visit
         cy.visit("/");
         addNewTodo("新しいTODO 1");
         // check
-        cy.get(`li > input[type="checkbox"]`).check();
+        cy.get("li > input[type=\"checkbox\"]").check();
         cy.get("li > s").should(items => {
             expect(items).to.have.length(1);
         });
         // uncheck
-        cy.get(`li > input[type="checkbox"]`).uncheck();
+        cy.get("li > input[type=\"checkbox\"]").uncheck();
         cy.get("li > s").should(items => {
             expect(items).to.have.length(0);
         });
