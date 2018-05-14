fa211987b58427fc5558a20653b995fcb7530647
nd-02110114 <nd.12021218@gmail.com>
2018-05-14 22:52:37 +0900

    feat: arrange sw register script

---
+++ source/sw.js
@@ -0,0 +1,10 @@
+// workbox init setting
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+workbox.core.setCacheNameDetails({ prefix: "js-primer-v1" });
+workbox.googleAnalytics.initialize();
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute([]);
