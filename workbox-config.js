e3193edf7091d633c8ac65508c575ffcf75a642c
nd-02110114 <nd.12021218@gmail.com>
2018-05-14 22:51:19 +0900

    feat: create workbox configuration

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    globDirectory: "_book",
+    globPatterns: [
+        "**/*.{js,css,html,png,eot,svg,ttf,woff,woff2,otf,,ico}"
+    ],
+    swSrc: "source/sw.js",
+    swDest: "_book/sw.js",
+};
