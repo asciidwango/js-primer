f3f928561ef44b5945a63b6f97d8dd26435db3ff
azu <azuciao@gmail.com>
2018-06-23 17:29:55 +0900

    fix(class): use {{book.esversion}}

--- tools/strip-markdown-extension.js
+++ tools/strip-markdown-extension.js
@@ -2,6 +2,7 @@
 "use strict";
 const execFileSync = require("child_process").execFileSync;
 const includeCodeBlock = require.resolve(".bin/include-codeblock");
+const bookJSON = require("../book.json");
 /**
  * contentからからjs-primerで使ってるMarkdownの拡張を取り除く
  * @param {string} content
@@ -9,7 +10,9 @@ const includeCodeBlock = require.resolve(".bin/include-codeblock");
  */
 function stripMarkdownExtension(content) {
     // {{book.console}} を取り除く
-    content = content.replace(/^{{book\.console}}$/gm, "");
+    content = content.replace(/^{{\s*?book\.console\s*?}}$/gm, "");
+    // {{book.esversion}} 置換
+    content = content.replace(/{{\s*?book\.esversion\s*?}}/gm, bookJSON.variables.esversion);
     // ## タイトル {#id}
     // GitBookのid拡張を取り除く
     content = content.replace(/^(#+.*){#.*}/gm, "$1");
