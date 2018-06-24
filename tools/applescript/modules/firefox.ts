554f017616d462a2565aa975b119306d2e73d236
azu <azuciao@gmail.com>
2018-06-24 19:00:41 +0900

    chore(todoapp): JXAを更新 && スクリーンショットを更新

--- tools/applescript/modules/firefox.ts
+++ tools/applescript/modules/firefox.ts
@@ -1,39 +1,39 @@
-const runJxa: (fn: (...args: any[]) => any) => (...args: any[]) => Promise<void> = require('osa2');
+import { run } from "@jxa/run";
 
 export function launchFirefox({ url, profileName }: { url: string, profileName: string }) {
-    return runJxa((url, profileName) => {
+    return run((url, profileName) => {
         var app = Application.currentApplication();
         app.includeStandardAdditions = true;
-        app.doShellScript(`open -a /Applications/FirefoxDeveloperEdition.app --args -no-remote -CreateProfile '${profileName}'`);
-        app.doShellScript(`open -a /Applications/FirefoxDeveloperEdition.app --args -private-window '${url}' -no-remote -P '${profileName}'`);
-    })(url, profileName);
+        app.doShellScript(`open -a /Applications/Firefox.app --args -no-remote -CreateProfile '${profileName}'`);
+        app.doShellScript(`open -a /Applications/Firefox.app --args -private-window '${url}' -no-remote -P '${profileName}'`);
+    }, url, profileName);
 }
 
 export function setFirefoxWindowBounds() {
-    return runJxa(() => {
-        const Firefox = Application("FirefoxDeveloperEdition");
+    return run(() => {
+        const Firefox = Application("Firefox");
         Firefox.activate();
         Firefox.windows[0].bounds = { "x": 0, "y": 0, "width": 1280, "height": 720 }
-    })();
+    });
 }
 
 export function quitFirefox() {
-    return runJxa(() => {
-        const Firefox = Application("FirefoxDeveloperEdition");
+    return run(() => {
+        const Firefox = Application("Firefox");
         Firefox.quit();
-    })();
+    });
 }
 
 
 export function screenshotFirefox(outputFilePath: string) {
-    return runJxa((outputFilePath) => {
+    return run((outputFilePath) => {
         const app = Application.currentApplication();
         app.includeStandardAdditions = true;
-        const Firefox = Application("FirefoxDeveloperEdition");
+        const Firefox = Application("Firefox");
         Firefox.activate();
         delay(0.3);
         const window = Firefox.windows[0];
         const windowId = String(window.id());
         app.doShellScript(`/usr/sbin/screencapture -l ${windowId} "${outputFilePath}"`);
-    })(outputFilePath)
+    }, outputFilePath);
 }
