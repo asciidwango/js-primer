ffdeed1410c797e092e7dfbfae37a947267b0b3d
azu <azuciao@gmail.com>
2018-07-15 00:18:47 +0900

    fix(todoapp): fix final source

--- source/use-case/todoapp/final/final/index.js
+++ source/use-case/todoapp/final/final/index.js
@@ -1,9 +1,4 @@
 import { App } from "./src/App.js";
 
 const app = new App();
-window.addEventListener("load", () => {
-    app.mount();
-});
-window.addEventListener("unload", () => {
-    app.release();
-});
+app.mount();
