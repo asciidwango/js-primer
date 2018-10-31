02fe8d12e052cea013ad33c5154f619d3e3ac820
azu <azuciao@gmail.com>
2018-10-31 16:04:49 +0900

    fixup!

--- source/use-case/todoapp/event-model/event-emitter/src/EventEmitter.example.js
+++ source/use-case/todoapp/event-model/event-emitter/src/EventEmitter.example.js
@@ -3,8 +3,8 @@ const event = new EventEmitter();
 // イベントリスナー（コールバック関数）を登録
 event.addEventLister("test-event", () => console.log("One!"));
 event.addEventLister("test-event", () => console.log("Two!"));
-// コールバック関数をまとめて呼びだす
+// イベントをディスパッチする
 event.emit("test-event");
-// コールバック関数がそれぞれ呼び出し、コンソールには次のように出力される
+// コールバック関数がそれぞれ呼びだされ、コンソールには次のように出力される
 // "One!"
 // "Two!"
