6f4701c5f0d59ec6d31893c44a9404bfa4e0aa95
miy4 <miy4t7l@gmail.com>
2018-12-11 03:55:14 +0900

    fix(loop): fix typo

--- source/basic/loop/src/break/find-even-break-example.js
+++ source/basic/loop/src/break/find-even-break-example.js
@@ -4,15 +4,15 @@ function isEven(number) {
 }
 // `numbers`に偶数が含まれているならtrueを返す
 function isEvenIncluded(numbers) {
-    let isEventIncluded = false;
+    let isEvenIncluded = false;
     for (let i = 0; i < numbers.length; i++) {
         const number = numbers[i];
         if (isEven(number)) {
-            isEventIncluded = true;
+            isEvenIncluded = true;
             break;
         }
     }
-    return isEventIncluded;
+    return isEvenIncluded;
 }
 const array = [1, 5, 10, 15, 20];
 console.log(isEvenIncluded(array)); // => true
