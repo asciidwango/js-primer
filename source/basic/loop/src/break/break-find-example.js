98fec9088381b9e4eb56aeaa34937c78de2c05cf
miy4 <miy4@users.noreply.github.com>
2018-12-11 21:38:13 +0900

    fix(loop): fix typo (#582)

--- source/basic/loop/src/break/break-find-example.js
+++ source/basic/loop/src/break/break-find-example.js
@@ -1,11 +1,11 @@
 const numbers = [1, 5, 10, 15, 20];
 // 偶数があるかどうか
-let isEventIncluded = false;
+let isEvenIncluded = false;
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
     if (number % 2 === 0) {
-        isEventIncluded = true;
+        isEvenIncluded = true;
         break;
     }
 }
-console.log(isEventIncluded); // => true
+console.log(isEvenIncluded); // => true
--- source/basic/loop/src/break/find-even-return-example.js
+++ source/basic/loop/src/break/find-even-return-example.js
@@ -1,7 +1,7 @@
 function isEven(number) {
     return number % 2 === 0;
 }
-function isEventIncluded(numbers) {
+function isEvenIncluded(numbers) {
     for (let i = 0; i < numbers.length; i++) {
         const number = numbers[i];
         if (isEven(number)) {
@@ -11,4 +11,4 @@ function isEventIncluded(numbers) {
     return false;
 }
 const numbers = [1, 5, 10, 15, 20];
-console.log(isEventIncluded(numbers)); // => true
+console.log(isEvenIncluded(numbers)); // => true
