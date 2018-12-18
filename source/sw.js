7defd2f5e9c002ed11b824ec05dddcdaec0040fc
azu <azuciao@gmail.com>
2018-12-18 19:56:02 +0900

    fix(website): update workbox

--- source/sw.js
+++ source/sw.js
@@ -1,5 +1,5 @@
 // workbox init setting
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({ prefix: "js-primer-v1" });
 workbox.googleAnalytics.initialize();
