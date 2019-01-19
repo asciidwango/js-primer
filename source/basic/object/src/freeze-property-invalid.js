72d55580083cd6564f2337a57c20784b4f2de8c2
azu <azuciao@gmail.com>
2019-01-19 18:38:08 +0900

    add message

--- source/basic/object/src/freeze-property-invalid.js
+++ source/basic/object/src/freeze-property-invalid.js
@@ -1,4 +1,4 @@
 "use strict";
 const object = Object.freeze({ key: "value" });
 // freezeしたオブジェクトにプロパティを追加や変更できない
-object.key = "value"; // => TypeError
+object.key = "value"; // => TypeError: "key" is read-only
