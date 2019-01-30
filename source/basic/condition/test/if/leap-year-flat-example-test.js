454756413b8f6cc135aadfc1c508c76185ba5948
azu <azuciao@gmail.com>
2019-01-30 16:24:55 +0900

    fix test

--- source/basic/condition/test/if/leap-year-flat-example-test.js
+++ source/basic/condition/test/if/leap-year-flat-example-test.js
@@ -11,7 +11,7 @@ const testLeapYear = (year, expectedMessage) => {
     };
     const console = {
         log(message) {
-            assert.equal(message, expectedMessage, `year: ${year}`);
+            assert.equal(message, expectedMessage(year), `year: ${year}`);
         }
     };
     strictEval(Code, {
@@ -22,20 +22,22 @@ const testLeapYear = (year, expectedMessage) => {
 describe("leap-year-flat", function() {
     context("うるう年であるならば", function() {
         it("should return \"うるう年です\"", function() {
-            testLeapYear(0, "うるう年です");
-            testLeapYear(4, "うるう年です");
-            testLeapYear(400, "うるう年です");
-            testLeapYear(2000, "うるう年です");
-            testLeapYear(2016, "うるう年です");
-            testLeapYear(1289031804, "うるう年です");
+            const message = (year) => `${year}年はうるう年です`;
+            testLeapYear(0, message);
+            testLeapYear(4, message);
+            testLeapYear(400, message);
+            testLeapYear(2000, message);
+            testLeapYear(2016, message);
+            testLeapYear(1289031804, message);
         });
     });
     context("うるう年でないならば", function() {
         it("should return \"うるう年ではありません\"", function() {
-            testLeapYear(100, "うるう年ではありません");
-            testLeapYear(200, "うるう年ではありません");
-            testLeapYear(1234, "うるう年ではありません");
-            testLeapYear(2100, "うるう年ではありません");
+            const message = (year) => `${year}年はうるう年ではありません`;
+            testLeapYear(100, message);
+            testLeapYear(200, message);
+            testLeapYear(1234, message);
+            testLeapYear(2100, message);
         });
     });
 });
