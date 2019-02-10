d7c7a2fdc12f8be4c035e54853be77fc91e339af
azu <azuciao@gmail.com>
2019-02-10 18:03:06 +0900

    feat(tools): strip-markdown-extension.jsですべての変数置換に対応

--- tools/strip-markdown-extension.js
+++ tools/strip-markdown-extension.js
@@ -3,16 +3,20 @@
 const execFileSync = require("child_process").execFileSync;
 const includeCodeBlock = require.resolve(".bin/include-codeblock");
 const bookJSON = require("../book.json");
+
 /**
- * contentからからjs-primerで使ってるMarkdownの拡張を取り除く
+ * contentからjs-primerで使ってるMarkdownの拡張を取り除く
  * @param {string} content
  * @returns {string}
  */
 function stripMarkdownExtension(content) {
     // {{book.console}} を取り除く
     content = content.replace(/^{{\s*?book\.console\s*?}}$/gm, "");
-    // {{book.esversion}} 置換
-    content = content.replace(/{{\s*?book\.esversion\s*?}}/gm, bookJSON.variables.esversion);
+    // {{book.変数}} を 置換する
+    Object.keys(bookJSON.variables).forEach(key => {
+        const pattern = new RegExp(`{{\\s*?book\.${key}\*\?}}`, "gm");
+        content = content.replace(pattern, bookJSON.variables[key]);
+    });
     // ## タイトル {#id}
     // GitBookのid拡張を取り除く
     content = content.replace(/^(#+.*){#.*}/gm, "$1");
@@ -29,6 +33,6 @@ if (!module.parent) {
         throw new Error("node strip-markdown-extension.js /path/to/file.md");
     }
     const filePath = path.resolve(process.cwd(), process.argv[2]);
-    const includedContent = execFileSync('node', [includeCodeBlock, filePath]);
+    const includedContent = execFileSync("node", [includeCodeBlock, filePath]);
     console.log(stripMarkdownExtension(String(includedContent)));
 }
