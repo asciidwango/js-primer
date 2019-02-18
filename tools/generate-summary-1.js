6ec984a8c2f11ff3f95349bfab805851fbdf6e84
azu <azuciao@gmail.com>
2019-02-18 09:34:35 +0900

    feat(basic): 第一部の目次を追加

--- tools/generate-summary-1.js
+++ tools/generate-summary-1.js
@@ -1,33 +1,37 @@
 const fs = require("fs");
 const path = require("path");
-const getFilePathListAsync = require("gitbook-summary-to-path").getFilePathListAsync;
+const { getChapterListAsync } = require("gitbook-summary-to-path");
 const sourceDir = path.join(__dirname, "../source");
 const OUTLINE = path.join(sourceDir, "README.md");
-const meow = require('meow');
-const parseFrontMatter = require('front-matter');
+const BasicIndex = path.join(sourceDir, "basic");
+const meow = require("meow");
+const parseFrontMatter = require("front-matter");
 const cli = meow(`
     第一部のサマリを作成するツール
 
     Usage
       $ node generate-summary-1.js
 `, {
-        flags: {
-        },
-        autoVersion: true,
-        autoHelp: true
-    });
+    flags: {},
+    autoVersion: true,
+    autoHelp: true
+});
 
-(async function () {
-    const summaryList = await getFilePathListAsync(OUTLINE);
-    const filePathList = summaryList.filter(filePath => filePath !== OUTLINE);
-    filePathList.forEach(filePath => {
+(async function() {
+    const summaryDir = path.dirname(OUTLINE);
+    const summaryList = await getChapterListAsync(OUTLINE);
+    const chapterList = summaryList.filter(chapter => {
+        return path.resolve(summaryDir, chapter.path) !== OUTLINE;
+    });
+    chapterList.forEach(chapter => {
+        const filePath = path.resolve(path.dirname(OUTLINE), chapter.path);
         const inputText = fs.readFileSync(filePath, "utf-8");
         const frontMatter = parseFrontMatter(inputText);
-        if (frontMatter.attributes.description){
-            console.log(`- [${filePath}](${filePath})
+        if (frontMatter.attributes.description) {
+            console.log(`### [${chapter.title}](${path.relative(BasicIndex, filePath)})
 
 ${frontMatter.attributes.description}
-`)
+`);
         }
     });
 })();
