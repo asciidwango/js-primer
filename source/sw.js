f9b017a6c25b394ee11287c6f8fbb6d26f60f61d
azu <azuciao@gmail.com>
2019-04-27 22:06:28 +0900

    fix(website): reload asap

--- source/sw.js
+++ source/sw.js
@@ -2,11 +2,10 @@
 importScripts(
     "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js"
 );
-importScripts(
-    "https://unpkg.com/service-worker-updatefound-refresh-dialog@1.1.0/dist/service-worker-updatefound-refresh-dialog.umd.js"
-);
-
 workbox.core.setCacheNameDetails({ prefix: "js-primer-v1" });
 workbox.googleAnalytics.initialize();
+// reload asap
+workbox.skipWaiting();
+workbox.clientsClaim();
 // precache
 workbox.precaching.precacheAndRoute([]);
