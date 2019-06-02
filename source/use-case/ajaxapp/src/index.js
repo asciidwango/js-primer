607a523925a88beb79f368d5e076d8dcbac081d9
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-06-02 17:34:44 +0900

    refactor(usecase/ajaxapp): ajaxappのサンプルアプリをfetch+async/awaitに書き換え

--- source/use-case/ajaxapp/src/index.js
+++ source/use-case/ajaxapp/src/index.js
@@ -1,35 +1,31 @@
-function main() {
-    const userId = getUserId();
-    getUserInfo(userId)
-        .then((userInfo) => createView(userInfo))
-        .then((view) => displayView(view))
-        .catch((error) => {
-            console.error(`エラーが発生しました (${error})`);
-        });
-}
-
-function getUserInfo(userId) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open("GET", `https://api.github.com/users/${userId}`);
-        request.addEventListener("load", (event) => {
-            if (event.target.status !== 200) {
-                reject(new Error(`${event.target.status}: ${event.target.statusText}`));
-            }
-
-            const userInfo = JSON.parse(event.target.responseText);
-            resolve(userInfo);
-        });
-        request.addEventListener("error", () => {
-            reject(new Error("ネットワークエラー"));
-        });
-        request.send();
-    });
-}
-
 function getUserId() {
-    const value = document.getElementById("userId").value;
+    const value = document.getElementById('userId').value;
     return encodeURIComponent(value);
+}
+
+function fetchUserInfo(userId) {
+    const url = `https://api.github.com/users/${userId}`;
+    return fetch(url).then(response => response.json());
+}
+
+function escapeSpecialChars(str) {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
+
+function escapeHTML(strings, ...values) {
+    return strings.reduce((result, string, i) => {
+        const value = values[i - 1];
+        if (typeof value === 'string') {
+            return result + escapeSpecialChars(value) + string;
+        } else {
+            return result + String(value) + string;
+        }
+    });
 }
 
 function createView(userInfo) {
@@ -46,26 +42,17 @@ function createView(userInfo) {
 }
 
 function displayView(view) {
-    const result = document.getElementById("result");
+    const result = document.getElementById('result');
     result.innerHTML = view;
 }
 
-function escapeSpecialChars(str) {
-    return str
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
-}
-
-function escapeHTML(strings, ...values) {
-    return strings.reduce((result, string, i) => {
-        const value = values[i - 1];
-        if (typeof value === "string") {
-            return result + escapeSpecialChars(value) + string;
-        } else {
-            return result + String(value) + string;
-        }
-    });
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch (error) {
+        console.error(`エラーが発生しました (${error})`);
+    }
 }
