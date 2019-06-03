51a023982bd49f800404057c171242f6da53af5c
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-06-03 16:42:03 +0900

    refactor(usecase/ajaxapp): ajaxappのサンプルアプリをfetch+async/awaitに書き換え (#819)
    * refactor(usecase/ajaxapp): ajaxappのサンプルアプリをfetch+async/awaitに書き換え
    
    * lintfix

--- source/use-case/ajaxapp/src/index.js
+++ source/use-case/ajaxapp/src/index.js
@@ -1,53 +1,11 @@
-function main() {
-    const userId = getUserId();
-    getUserInfo(userId)
-        .then((userInfo) => createView(userInfo))
-        .then((view) => displayView(view))
-        .catch((error) => {
-            console.error(`エラーが発生しました (${error})`);
-        });
-}
-
-function getUserInfo(userId) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.open("GET", `https://api.github.com/users/${userId}`);
-        request.addEventListener("load", (event) => {
-            if (event.target.status !== 200) {
-                reject(new Error(`${event.target.status}: ${event.target.statusText}`));
-            }
-
-            const userInfo = JSON.parse(event.target.responseText);
-            resolve(userInfo);
-        });
-        request.addEventListener("error", () => {
-            reject(new Error("ネットワークエラー"));
-        });
-        request.send();
-    });
-}
-
 function getUserId() {
     const value = document.getElementById("userId").value;
     return encodeURIComponent(value);
 }
 
-function createView(userInfo) {
-    return escapeHTML`
-    <h4>${userInfo.name} (@${userInfo.login})</h4>
-    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-    <dl>
-        <dt>Location</dt>
-        <dd>${userInfo.location}</dd>
-        <dt>Repositories</dt>
-        <dd>${userInfo.public_repos}</dd>
-    </dl>
-    `;
-}
-
-function displayView(view) {
-    const result = document.getElementById("result");
-    result.innerHTML = view;
+function fetchUserInfo(userId) {
+    const url = `https://api.github.com/users/${userId}`;
+    return fetch(url).then(response => response.json());
 }
 
 function escapeSpecialChars(str) {
@@ -68,4 +26,33 @@ function escapeHTML(strings, ...values) {
             return result + String(value) + string;
         }
     });
+}
+
+function createView(userInfo) {
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+    const result = document.getElementById("result");
+    result.innerHTML = view;
+}
+
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch (error) {
+        console.error(`エラーが発生しました (${error})`);
+    }
 }
