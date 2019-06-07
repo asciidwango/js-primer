886122091dd3c541a2dfb0ac7921fc9cdb74c9cb
azu <azuciao@gmail.com>
2019-06-07 20:24:06 +0900

    fix(todo): fix JSDoc
    fix #820

--- source/use-case/todoapp/final/create-view/src/view/TodoListView.js
+++ source/use-case/todoapp/final/create-view/src/view/TodoListView.js
@@ -6,7 +6,7 @@ export class TodoListView {
      * `todoItems`に対応するTodoリストのHTML要素を作成して返す
      * @param {TodoItemModel[]} todoItems TodoItemModelの配列
      * @param {function({id:string, completed: boolean})} onUpdateTodo チェックボックスの更新イベントリスナー
-     * @param {function({id:string)}} onDeleteTodo 削除ボタンのクリックイベントリスナー
+     * @param {function({id:string})} onDeleteTodo 削除ボタンのクリックイベントリスナー
      * @returns {Element} TodoItemModelの配列に対応したリストのHTML要素
      */
     createElement(todoItems, { onUpdateTodo, onDeleteTodo }) {
