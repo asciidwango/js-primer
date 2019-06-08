b2b4a32f26f67b5003d8fd09d4676864be6cf92f
azu <azuciao@gmail.com>
2019-06-08 17:58:05 +0900

    refactor(variables): const,let,varの順番で書き直し

--- source/basic/variables/src/const-do-not-assign-invalid.js
+++ source/basic/variables/src/const-do-not-assign-invalid.js
@@ -1,2 +1,2 @@
 const bookTitle = "JavaScriptの本";
-bookTitle = "上書き"; // TypeError: invalid assignment to const `bookTitle'
+bookTitle = "新しいタイトル"; // => TypeError: invalid assignment to const `bookTitle'
