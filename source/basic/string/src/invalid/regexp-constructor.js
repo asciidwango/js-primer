e77c03ab18d94f2038c339c76db317a915649e54
azu <azuciao@gmail.com>
2019-07-21 21:00:12 +0900

    use [ instead of +

--- source/basic/string/src/invalid/regexp-constructor.js
+++ source/basic/string/src/invalid/regexp-constructor.js
@@ -1,7 +1,7 @@
 // `RegExp`コンストラクタは実行時にパターンが評価され、例外が発生する
 function main(){
-    // `+`は繰り返しを意味する特殊文字であるため、単独で書けない
-    const invalidPattern = new RegExp("+");
+    // `[`は対となる`]`を組み合わせる特殊文字であるため、単独で書けない
+    const invalidPattern = new RegExp("[");
 }
 
 // `main`関数を呼び出すことで初めて例外が発生する
