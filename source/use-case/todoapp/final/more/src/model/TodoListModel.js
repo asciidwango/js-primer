f66331236c39d71cd71de7f290658c39c3a3bc84
yinm <yinm.cminor9@gmail.com>
2019-08-10 14:53:33 +0900

    fix: 未定義エラーになっていた関数を定義する (EventListenerの解除)

--- source/use-case/todoapp/final/more/src/model/TodoListModel.js
+++ source/use-case/todoapp/final/more/src/model/TodoListModel.js
@@ -34,6 +34,14 @@ export class TodoListModel extends EventEmitter {
     }
 
     /**
+     * TodoListの状態が更新されたときに呼び出されるリスナー関数を解除する
+     * @param {Function} listener
+     */
+    offChange(listener) {
+        this.removeEventListener("change", listener);
+    }
+
+    /**
      * 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
      */
     emitChange() {
