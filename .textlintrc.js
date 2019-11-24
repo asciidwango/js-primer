69d4e7cb4b775bbff79e6a0a767c59ea57af23eb
azu <azu@users.noreply.github.com>
2019-11-24 21:36:14 +0900

    fix(prh): インターフェースに統一 (#1003)

--- .textlintrc.js
+++ .textlintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     }
   },
   "rules": {
+    "@textlint-ja/no-synonyms": {
+      "allows": ["アプリ", "ウェブアプリ"]
+    },
     // 使っては行けない変数、関数名
     // 詳細: https://github.com/asciidwango/js-primer/issues/804
     "inline-code-denylist": {
