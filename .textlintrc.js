f875ed1d2676ef1c205bffa9044570ff602b9ae8
azu <azuciao@gmail.com>
2019-12-20 21:48:41 +0900

    fix

--- .textlintrc.js
+++ .textlintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     "preset-ja-technical-writing": {
       "sentence-length": {
-        "max": 90
+        "max": 92 
       },
       "no-exclamation-question-mark": {
         "allowFullWidthQuestion": true
