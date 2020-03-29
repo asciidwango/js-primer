0d590320d58c99843c7c2ba600fc3e50e0d0cc86
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-03-29 16:26:06 +0900

    fix(usecase/nodecli): encodingの指定方法を統一

--- source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
+++ source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
@@ -4,15 +4,15 @@ const path = require("path");
 const md2html = require("../md2html");
 
 it("converts Markdown to HTML (GFM=false)", () => {
-    const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), "utf8");
-    const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected.html"), "utf8");
+    const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
+    const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected.html"), { encoding: "utf8" });
 
     assert.strictEqual(md2html(sample, { gfm: false }), expected);
 });
 
 it("converts Markdown to HTML (GFM=true)", () => {
-    const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), "utf8");
-    const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected-gfm.html"), "utf8");
+    const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
+    const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected-gfm.html"), { encoding: "utf8" });
 
     assert.strictEqual(md2html(sample, { gfm: true }), expected);
 });
