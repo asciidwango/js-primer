0acc55d1b68e88b75c6721424a29b16f0857f17d
azu <azuciao@gmail.com>
2020-04-01 00:19:39 +0900

    fix(use-case/nodecli): fix bootstrap script

--- source/use-case/nodecli/bootstrap.sh
+++ source/use-case/nodecli/bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# variable
-declare scriptDir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-declare nodecliDir="$(scriptDir)"
-
+declare nodecliDir=$(pwd)
 # npm install
 cd "${nodecliDir}/argument-parse/src" && npm install
 cd "${nodecliDir}/md-to-html/src" && npm install
