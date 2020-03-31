2d6b48f44dae083124e57f6efab6340c4e7f6166
azu <azuciao@gmail.com>
2020-04-01 00:15:53 +0900

    test(use-case/nodecli): add --gfm test

--- source/use-case/nodecli/test/test.js
+++ source/use-case/nodecli/test/test.js
@@ -4,40 +4,42 @@ const assert = require("assert");
 const execa = require("execa");
 const nodecliDir = path.join(__dirname, "../");
 const snapshotDir = path.join(__dirname, "snapshots");
+const duplicateGfm = (item) => {
+    return [
+        item,
+        // --gfmを足したバージョンを作成する
+        {
+            filePath: item.filePath,
+            args: item.args.concat("--gfm"),
+            outputId: item.filePath + "--gfm"
+        }
+    ];
+};
+const flatten = (array, depth) => {
+    const flattend = [];
+    (function flat(array, depth) {
+        for (const el of array) {
+            if (Array.isArray(el) && depth > 0) {
+                flat(el, depth - 1);
+            } else {
+                flattend.push(el);
+            }
+        }
+    })(array, Math.floor(depth) || 1);
+    return flattend;
+};
 /**
- * テスト対象とargsを指定する
- * @type {({filePath: string}|{args: [string], filePath: string})[]}
+ * --gfm を実装してるファイルは--gfm版もテスト対象にする
+ * 配列の配列になってるので1段flat化
  */
-const testTargetFileList = [
+const gfmItems = flatten([
+    // これらは--gfmを実装しているので、--gfmをテストする
     {
-        filePath: path.join(nodecliDir, "helloworld/src/main.js"),
-    },
-    {
-        filePath: path.join(nodecliDir, "argument-parse/src/cjs-import.js"),
-    },
-    // nodeのパスが入るので扱いにくい
-    // {
-    //     filePath: path.join(nodecliDir, "argument-parse/src/main-1.js"),
-    //     args: ["sample.md"]
-    // },
-    {
-        filePath: path.join(nodecliDir, "argument-parse/src/main-2.js"),
+        filePath: path.join(nodecliDir, "md-to-html/src/main-3.js"),
         args: ["sample.md"]
     },
     {
         filePath: path.join(nodecliDir, "md-to-html/src/main.js"),
-        args: ["sample.md"]
-    },
-    {
-        filePath: path.join(nodecliDir, "md-to-html/src/main-1.js"),
-        args: ["sample.md"]
-    },
-    {
-        filePath: path.join(nodecliDir, "md-to-html/src/main-2.js"),
-        args: ["sample.md"]
-    },
-    {
-        filePath: path.join(nodecliDir, "md-to-html/src/main-3.js"),
         args: ["sample.md"]
     },
     {
@@ -58,12 +60,45 @@ const testTargetFileList = [
     },
     {
         filePath: path.join(nodecliDir, "refactor-and-unittest/src/main-last.js"),
-        args: ["--gfm", "sample.md"]
+        args: ["sample.md"]
+    }
+].map(item => duplicateGfm(item)));
+
+/**
+ * テスト対象とargsを指定する
+ * outputIdはオプショナルで、指定した場合はスナップショットのファイル名に使う
+ */
+const testTargetFileList = [
+    {
+        filePath: path.join(nodecliDir, "helloworld/src/main.js"),
     },
-];
+    {
+        filePath: path.join(nodecliDir, "argument-parse/src/cjs-import.js"),
+    },
+    // nodeのパスが入るので扱いにくい
+    // {
+    //     filePath: path.join(nodecliDir, "argument-parse/src/main-1.js"),
+    //     args: ["sample.md"]
+    // },
+    {
+        filePath: path.join(nodecliDir, "argument-parse/src/main-2.js"),
+        args: ["sample.md"]
+    },
+    {
+        filePath: path.join(nodecliDir, "md-to-html/src/main-1.js"),
+        args: ["sample.md"]
+    },
+    {
+        filePath: path.join(nodecliDir, "md-to-html/src/main-2.js"),
+        args: ["sample.md"]
+    },
+].concat(gfmItems);
+
 describe("use-case/nodecli", function() {
-    testTargetFileList.forEach(({ filePath, args }) => {
-        it(`snapshot test ${filePath}`, async function() {
+    testTargetFileList.forEach(({ filePath, args, outputId }) => {
+        const testFileId = outputId ? outputId : filePath;
+        const testFileName = path.relative(nodecliDir, testFileId);
+        it(`snapshot test ${testFileName}`, async function() {
             const fileArgs = args ? args : [];
             const { stdout } = await execa("node", [filePath].concat(fileArgs), {
                 cwd: path.dirname(filePath)
@@ -71,7 +106,7 @@ describe("use-case/nodecli", function() {
             // ユーザーパスは省略する
             // /path/to/nodecli/arguments/src/main.js -> <nodecli>/arguments/src/main.js
             const normalizedStdout = (stdout || "").split(nodecliDir).join("<nodecli>/");
-            const fileName = path.relative(nodecliDir, filePath).split(path.sep).join("_");
+            const fileName = testFileName.split(path.sep).join("_");
             const expectedFilePath = path.join(snapshotDir, fileName + ".txt");
             // Usage: update snapshots
             // UPDATE_SNAPSHOT=1 npm test
