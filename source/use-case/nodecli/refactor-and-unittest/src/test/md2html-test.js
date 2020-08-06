99485bfa4c062e2fec8349ae601a7312adfa1f05
sasahara <sasahar@intra-mart.jp>
2020-08-06 18:52:16 +0900

    fix_cli_test_code 末尾の改行コードを削除

--- source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
+++ source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
@@ -8,13 +8,13 @@ it("converts Markdown to HTML (GFM=false)", () => {
     const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
     const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected.html"), { encoding: "utf8" });
 
-    assert.strictEqual(md2html(sample, { gfm: false }), expected);
+    assert.strictEqual(md2html(sample, { gfm: false }).trim(), expected);
 });
 
 it("converts Markdown to HTML (GFM=true)", () => {
     const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
     const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected-gfm.html"), { encoding: "utf8" });
 
-    assert.strictEqual(md2html(sample, { gfm: true }), expected);
+    assert.strictEqual(md2html(sample, { gfm: true }).trim(), expected);
 });
 
