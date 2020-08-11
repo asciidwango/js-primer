b5fd61b4de229e1d2772584a45a39f4c1410bd84
sasahara-study <69297204+sasahara-study@users.noreply.github.com>
2020-08-11 10:22:48 +0900

    fix(nodecli): 末尾の改行コードを削除 (#1234)
    * fix_cli_test_code 末尾の改行コードを削除
    
    * fix(nodecli) 末尾の改行コードを削除
    
    trimEnd()に変更、コメントとexpectedに対しての末尾空白行の削除を追加
    
    * fix(nodecli) 対象にユニットテストを追加
    
    Co-authored-by: sasahara <sasahar@intra-mart.jp>

--- source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
+++ source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
@@ -7,14 +7,14 @@ it("converts Markdown to HTML (GFM=false)", () => {
     // fs.readFileSyncは同期的にファイルを読み込むメソッド
     const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
     const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected.html"), { encoding: "utf8" });
-
-    assert.strictEqual(md2html(sample, { gfm: false }), expected);
+    // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較しています
+    assert.strictEqual(md2html(sample, { gfm: false }).trimEnd(), expected.trimEnd());
 });
 
 it("converts Markdown to HTML (GFM=true)", () => {
     const sample = fs.readFileSync(path.resolve(__dirname, "./fixtures/sample.md"), { encoding: "utf8" });
     const expected = fs.readFileSync(path.resolve(__dirname, "./fixtures/expected-gfm.html"), { encoding: "utf8" });
-
-    assert.strictEqual(md2html(sample, { gfm: true }), expected);
+    // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較しています
+    assert.strictEqual(md2html(sample, { gfm: true }).trimEnd(), expected.trimEnd());
 });
 
