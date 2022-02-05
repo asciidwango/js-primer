76c75cb36026c27a4b089f21a61fc764ae6c5d07
azu <azuciao@gmail.com>
2022-02-05 17:15:31 +0900

    fix

--- textlint/textlint-rule-no-use-prototype-hash/index.js
+++ textlint/textlint-rule-no-use-prototype-hash/index.js
@@ -1,14 +1,18 @@
-let report = (context, options = {}) => {
+const report = (context, options = {}) => {
     const { Syntax, RuleError, report, getSource, fixer } = context;
     return {
         [Syntax.Code](node) {
             const text = getSource(node);
+            const isInHeader = node.parent.type === "Header";
+            // `#property` Private Fieldsは除外される
             const matches = text.matchAll(/^`(?<parent>\w+)#(?<property>\w+)`$/gu);
             for (const match of matches) {
                 const index = match.index || 0;
                 const parent = match.groups.parent;
                 const property = match.groups.property;
-                const replacedText = `${parent}の\`${property}\``;
+                const replacedText = isInHeader
+                    ? `\`${parent}.prototype.${property}\``
+                    : `${parent}の\`${property}\``;
                 report(node, new RuleError(`#をprototypeの短縮表記として使わないください。
                 
 Array#push は「Arrayの\`push\`メソッド」と表現します。
