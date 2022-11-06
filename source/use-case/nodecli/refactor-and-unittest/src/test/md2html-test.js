495a5f0cba535268b838fe2f9757a47d214d7432
azu <azu@users.noreply.github.com>
2022-11-06 15:46:07 +0900

    refactor(nodecli): Node CLIの章のリファクタリング (#1522)
    * refactor(nodecli): cd コマンドを追加
    
    * package-lockの注記を削除
    
    流石にデフォルトとなっているため
    
    * 相互運用性の補足
    
    * fix(nodecli): Promiseのエラーハンドリングがコールバックのままだった
    
    * fix
    
    * awaitの説明とitの説明を追加
    
    * feat: テストの失敗したケースを追加
    
    * Update source/use-case/nodecli/refactor-and-unittest/README.md
    
    Co-authored-by: Suguru Inatomi <suguru.inatomi@gmail.com>
    
    * fix
    
    * エラーと明確化
    
    Co-authored-by: Suguru Inatomi <suguru.inatomi@gmail.com>

--- source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
+++ source/use-case/nodecli/refactor-and-unittest/src/test/md2html-test.js
@@ -3,7 +3,7 @@ import * as fs from "node:fs/promises";
 import { md2html } from "../md2html.js";
 
 it("converts Markdown to HTML (GFM=false)", async() => {
-    // fs.readFileSyncは同期的にファイルを読み込むメソッド
+    // fs.readFileはPromiseを返すので、`await`式で読み込みが完了するまで待って内容を取得する
     const sample = await fs.readFile("test/fixtures/sample.md", { encoding: "utf8" });
     const expected = await fs.readFile("test/fixtures/expected.html", { encoding: "utf8" });
     // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較しています
