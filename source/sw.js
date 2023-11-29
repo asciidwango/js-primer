1f2cc864a168550ece891b220dc8e8b8b6543a83
azu <azuciao@gmail.com>
2023-11-29 13:25:02 +0900

    chore: update cache
    Signed-off-by: azu <azuciao@gmail.com>

--- source/sw.js
+++ source/sw.js
@@ -2,7 +2,7 @@
 importScripts(
     "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js"
 );
-workbox.core.setCacheNameDetails({ prefix: "js-primer-v1" });
+workbox.core.setCacheNameDetails({ prefix: "js-primer-v1.1" });
 workbox.googleAnalytics.initialize();
 // reload asap
 workbox.skipWaiting();
