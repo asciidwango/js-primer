6f53a79df51bb42ddd7439ad249cb81ca582643c
azu <azuciao@gmail.com>
2025-07-07 08:57:06 +0900

    remove Node.js 20

--- source/basic/iterator-generator/examples/basic/iterator.example.js
+++ source/basic/iterator-generator/examples/basic/iterator.example.js
@@ -9,4 +9,4 @@ const iterator = numberGenerator();
 // nextメソッドを呼ぶと最初の値を返す
 console.log(iterator.next().value); // => 1
 // nextメソッドを呼ぶと次の値を返す
-console.log("次の値: " + iterator.next().value);// => 2
+console.log("次の値: " + iterator.next().value); // => "次の値: 2"
